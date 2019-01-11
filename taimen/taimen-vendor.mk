7b3953ed102eb1ef6c2aed2cbc9ef9f1b44b6c4d
pimpmaneaton <shaunsibu@gmail.com>
2019-01-10 19:23:26 -0700

    Cause.....why not
    Change-Id: If182b71ac0654c9bb4809c30f97c9e049c97f59d

--- taimen/taimen-vendor.mk
+++ taimen/taimen-vendor.mk
@@ -42,6 +42,7 @@ PRODUCT_COPY_FILES += \
     vendor/google/taimen/proprietary/etc/permissions/privapp-permissions-google.xml:system/etc/permissions/privapp-permissions-google.xml \
     vendor/google/taimen/proprietary/etc/permissions/privapp-permissions-wahoo.xml:system/etc/permissions/privapp-permissions-wahoo.xml \
     vendor/google/taimen/proprietary/etc/permissions/vzw_mvs_permissions.xml:system/etc/permissions/vzw_mvs_permissions.xml \
+    vendor/google/taimen/proprietary/framework/qti-telephony-common.jar:system/framework/qti-telephony-common.jar \
     vendor/google/taimen/proprietary/lib/com.qualcomm.qti.imsrtpservice@1.0.so:system/lib/com.qualcomm.qti.imsrtpservice@1.0.so \
     vendor/google/taimen/proprietary/lib/lib-imsvideocodec.so:system/lib/lib-imsvideocodec.so \
     vendor/google/taimen/proprietary/lib/lib-imsvt.so:system/lib/lib-imsvt.so \
@@ -113,5 +114,4 @@ PRODUCT_PACKAGES += \
     embmslibrary \
     libhwinfo \
     qcrilhook \
-    rcsimssettings \
-    SoundPicker
+    rcsimssettings
